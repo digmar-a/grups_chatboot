df86484a03a4241bdb967670a05283373c42711c
yadavanju23 <yadavanju2344@gmail.com>
2026-05-08 11:15:03 +0530

    structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="grups-chatboot",
+    version="0.1.0",
+    author="Anju yadav",
+    description="Grups Automation chaboot ",
+    author_email="digmar.web@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+       
+    ],
+)
